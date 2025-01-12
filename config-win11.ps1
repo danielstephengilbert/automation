35c60e68cc49c1e205052df7e6b5a636116ee139
Daniel Gilbert <121529996+danielstephengilbert@users.noreply.github.com>
2025-01-11 22:43:23 -0800

    *Actually* fixed tab to space issue...

--- config-win11.ps1
+++ config-win11.ps1
@@ -40,51 +40,51 @@ $shortcuts_dir    = "${home_dir}\desktop\Shortcuts"
 # You may have to adjust the sendkeys() args as required if the UI changes.
 # Alas, there is no way but WSH Shell (COM Object) to do this part.
 function increaseDisplayScale {
- 	explorer ms-settings:display
- 	start-sleep -seconds 2
- 	# Create Windows Script Host shell:
- 	# https://learn.microsoft.com/en-us/previous-versions/windows/
+  explorer ms-settings:display
+  start-sleep -seconds 2
+  # Create Windows Script Host shell:
+  # https://learn.microsoft.com/en-us/previous-versions/windows/
   # internet-explorer/ie-developer/windows-scripting/ahcz2kh6(v=vs.84)
- 	$shell = new-object -comobject wscript.shell
- 	start-sleep -seconds 1
- 	$shell.sendkeys("{tab 8}{down 1}")
- 	start-sleep -seconds 1
- 	$shell.sendkeys("%{f4}")
+  $shell = new-object -comobject wscript.shell
+  start-sleep -seconds 1
+  $shell.sendkeys("{tab 8}{down 1}")
+  start-sleep -seconds 1
+  $shell.sendkeys("%{f4}")
 }
 
 # Create a Shortcuts directory on the user home Desktop.
 function createShortcutsDir {
- 	mkdir -force "${shortcuts_dir}" | out-null
+  mkdir -force "${shortcuts_dir}" | out-null
 }
 
 # Install and configure Notepad++.
 # This is still a work in progress.
 # Will probably circle back to this at the end...
 function installNotepadPlusPlus {
-	# This command just accepts the agreement for msstore to use winget.
- 	winget search notepad --accept-source-agreements | out-null
- 	winget install notepad++
- 	$npp_program = cmd /c "dir /s/b c:\*notepad++.exe"
+  # This command just accepts the agreement for msstore to use winget.
+  winget search notepad --accept-source-agreements | out-null
+  winget install notepad++
+  $npp_program = cmd /c "dir /s/b c:\*notepad++.exe"
   # If the appdata config file is not found, try to uncomment the line below
   # to ensure it gets created by launching Notepad++ as the target user.
- 	# start-process $npp_program -credential $cred
- 	$shell = new-object -comobject wscript.shell
- 	start-sleep -seconds 1
- 	$shortcut = $shell.createshortcut("${shortcuts_dir}\Notepad++.lnk")
- 	$shortcut.targetpath = $npp_program
- 	$shortcut.hotkey = "Ctrl+Alt+N"
- 	$shortcut.save()
- 	$npp_config = "${env:appdata}\notepad++\config.xml"
- 	# Enable Dark Mode.
+  # start-process $npp_program -credential $cred
+  $shell = new-object -comobject wscript.shell
+  start-sleep -seconds 1
+  $shortcut = $shell.createshortcut("${shortcuts_dir}\Notepad++.lnk")
+  $shortcut.targetpath = $npp_program
+  $shortcut.hotkey = "Ctrl+Alt+N"
+  $shortcut.save()
+  $npp_config = "${env:appdata}\notepad++\config.xml"
+  # Enable Dark Mode.
   (get-content $npp_config) `
-  	-replace  'name="DarkMode" enable="no"', `
+    -replace  'name="DarkMode" enable="no"', `
               'name="DarkMode" enable="yes"' `
-  	|	set-content $npp_config
+    | set-content $npp_config
   # Convert tabs to spaces and set width to 2.
- 	(get-content $npp_config) `
-  	-replace  'name="TabSetting" replaceBySpace="no" size="4"', `
+  (get-content $npp_config) `
+    -replace  'name="TabSetting" replaceBySpace="no" size="4"', `
               'name="TabSetting" replaceBySpace="yes" size="2"' `
- 		| set-content $npp_config
+    | set-content $npp_config
 }
 
 # Begin main script logic.
